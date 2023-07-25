ff156168ce13175cd506c7a55678c75fe960b558
Tiffany Messer <tiffanymesser15@gmail.com>
2023-07-25 16:39:44 -0400

    Updated formatting

--- js/script.js
+++ js/script.js
@@ -6,20 +6,13 @@
 // * Make a function that checks if the quesiton is right 
 
 
-
 //Create an array of objects for the questions 
 //quesetion
 //answers
 //correct answrs 
 
 
-
-
-
 //Creating Variables that I will need 
-
-console.log("I'm working")
-
 var submitButton = document.querySelector(".submit")
 var theQuestion = document.querySelector(".question-box");
 var answerBox = document.querySelector(".answer-choices");
@@ -42,7 +35,6 @@ var carrectSpan = document.querySelector(".close-correct")
 
 class Question {
     constructor(text, choices, answer) {
-
         this.text = text;
         this.choices = choices;
         this.answer = answer
@@ -50,21 +42,18 @@ class Question {
     }
 
 
-    correctAnswer(choice) {
-
+    correctAnswer() {
         answer = []
         if (this.answer === choices) {
             alert("Go Team Venture!")
         } else {
-            alert("DID YOU THINK YOUR PITIFUL GUESSING WOULD BE ENOUGH TO FOOL THE MIGHTY MONARCH?")
+            alert("DID YOU THINK YOU WOULD FOOL THE MIGHTY MONARCH?!")
         }
     }
 
 }
 
-
 //Adding some new classes for my questions
-
 const question1 = new Question("Who is 21's best friend at the beginning of the series?", ["Doctor Venture", "24", "The Blue Morpho", "Doctor Girlfriend"], "24")
 const question2 = new Question("Who is Billy Quizboy's partner?", ["Brock Sampson", "The Pink Pilgrim", "Sergent Hatred", "Hunter Gathers"], "The Pink Pilgrim")
 const question3 = new Question("What is St. Cloud's super power?", ["Frogs with bricks", "Knife and a Bear Costume", "An Exorbitant Amount of Money", "Turns into a Bea"], "An Exorbitant Amount of Money")
@@ -76,108 +65,69 @@ const question8 = new Question("What is the name of Dr. Orpheus' Daughter?", ["T
 const question9 = new Question("Who was Dr. Girlfriend's ex boyfriend?", ["Professor Fantomas", "Hank", "Dr. Venture", "Phantom Limb"], "Phantom Limb")
 const question10 = new Question("Who is Hunter Gather's charachter based on?", ["Hunter S Thompson", "Letterkenny", "Ralph Steadman", "Gonzo"], "Hunter S Thompson")
 
-
-const questionList = [question1, question2, question3, question4, question5, question6, question7, question8, question9,question10]
+const questionList = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
 
 var questionListLength = questionList.length
 
-// const ventureBrosQuiz = new Quiz(questionList)
-
-
-
-
-
-console.log(questionList[1])
-
 //Trying to get the text into the html file
-
 function pushText(questionNumber) {
-    // option.style.innerText = questionIndex.choices
 
     theQuestion.innerHTML = questionList[questionNumber].text
 
-
     //Trying to create buttons for every choice in the choices array. 
-
     while (answerBox.firstChild) {
         answerBox.removeChild(answerBox.firstChild)
     }
 
     questionList[questionNumber].choices.forEach((choice, choiceIndex) => {
-        console.log('choice = ' + choice)
-        console.log(choiceIndex)
-
-
         var newButtons = document.createElement("button")
-        var text = document.createTextNode(`${choice}`)
+        
         newButtons.setAttribute("class", choice)
         newButtons.setAttribute("id", "hidden")
-        
-        var clickedButton = newButtons.appendChild(text)
 
         document.querySelector(".answer-choices").appendChild(newButtons)
 
         //Create a click functions that checks the answer 
-
         newButtons.addEventListener("click", function () { checkIfCorrect(choice) })
-      
 
-
-
-        
     });
-    
+
 
 }
 
 
 function checkIfCorrect(choice) {
-    
-    
-
     if (choice === questionList[questionNumber].answer) {
         correctAnswer.style.display = "block"
 
-        // span.addEventListener
-        
-        carrectSpan.onclick = function() {
+        carrectSpan.onclick = function () {
             correctAnswer.style.display = "none";
-          }
-
-
+        }
 
         score++
         console.log(score)
         scored.innerHTML = score;
-       
-        
+
+
     } else {
-    
         modal.style.display = "block"
 
-        // span.addEventListener
-        
-        span.onclick = function() {
+        span.onclick = function () {
             modal.style.display = "none";
-          }
+        }
     }
 
     console.log("clickedButtons")
     questionNumber++
     pushText(questionNumber)
-   
+
 }
 
-function endOfGame(){
-    if(questionList[9]){
+function endOfGame() {
+    if (questionList[9]) {
         alert("Thanks for playing")
-
-
     }
 }
-
-
-
 
 
 pushText(questionNumber)
